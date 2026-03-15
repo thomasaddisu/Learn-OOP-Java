376716bd59e2978b86f932727365fba345c506e3
V empire <thomas.addisu@aastustudent.edu.et>
2026-03-15 20:23:41 +0300

    11th day work with ArrayList

--- Generics/CustomArrayList.java
+++ Generics/CustomArrayList.java
@@ -5,7 +5,18 @@ import java.util.ArrayList;
 public class CustomArrayList {
     static void main() {
         ArrayList list = new ArrayList();
-        list.add(45);
+        list.add("Thomas");
+        list.add("Software Enginnering");
+        list.add(3);
+        list.add("AASTU");
         System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        list.add(2,3);
+        System.out.println(list);
+
+        list.addAll(list);
+        System.out.println(list);
+
     }
 }
